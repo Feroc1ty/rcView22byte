b9eb10a748012024be6c125bd6c3c6e8a7a8dbfc
Feroc1ty <v_ryk@mail.ru>
2022-03-22 17:43:14 +0300

    + разрешение для интернета + запускаем rcView

--- app/src/main/java/ru/rykunov/rcview22byte/MainActivity.kt
+++ app/src/main/java/ru/rykunov/rcview22byte/MainActivity.kt
@@ -2,10 +2,48 @@ package ru.rykunov.rcview22byte
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.lifecycle.ViewModelProviders
+import androidx.recyclerview.widget.LinearLayoutManager
+import ru.rykunov.rcview22byte.adapter.NewsAdapter
+import ru.rykunov.rcview22byte.databinding.ActivityMainBinding
+import ru.rykunov.rcview22byte.pojo.News
+import ru.rykunov.rcview22byte.viewmodel.HomeViewModel
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var binding: ActivityMainBinding
+    private lateinit var homeMvvm: HomeViewModel
+    lateinit var newsItemsAdapter: NewsAdapter
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        newsItemsAdapter = NewsAdapter(this)
+        setContentView(binding.root)
+
+        prepareNewsRcView()
+        homeMvvm = ViewModelProviders.of(this)[HomeViewModel::class.java]
+        homeMvvm.getNewsList()
+        observeNewsItemsLiveData()
+
+
+    }
+
+    /*
+    Подготовка Recycler View
+    */
+    private fun prepareNewsRcView() {
+        binding.rcNews.apply {
+            layoutManager = LinearLayoutManager(this@MainActivity, LinearLayoutManager.VERTICAL, false)
+            adapter = newsItemsAdapter
+        }
+    }
+
+    private fun observeNewsItemsLiveData() {
+        homeMvvm.observeNewsLiveData().observe(this
+        ) { newsList ->
+            newsItemsAdapter.setNews(newslistarray = newsList as ArrayList<News>)
+        }
+
     }
 }
