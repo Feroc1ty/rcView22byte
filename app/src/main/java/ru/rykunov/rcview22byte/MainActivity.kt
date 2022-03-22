fcc59e1a86a9d776547897804a9f2242dc0ce12d
Feroc1ty <v_ryk@mail.ru>
2022-03-22 16:56:30 +0300

    + Зависимости в gradle

---
+++ app/src/main/java/ru/rykunov/rcview22byte/MainActivity.kt
@@ -0,0 +1,11 @@
+package ru.rykunov.rcview22byte
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
